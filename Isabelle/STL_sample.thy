2ac09dd16368b7c955d03f3052067089342a6c4f
Mark Chevallier <mchevall@kyneth.inf.ed.ac.uk>
2023-05-02 13:53:25 +0100

    Trying to redefine robust to avoid any drops or truncations on the list; finishing termination proof

--- Isabelle/STL_sample.thy
+++ Isabelle/STL_sample.thy
@@ -34,6 +34,14 @@ proof -
     by auto
 qed
 
+lemma signal_shift_exists:
+  fixes t :: "(real \<times> 'v::real_vector) list" and r :: real
+  assumes "\<exists>n<length t. fst (t!n) = r"
+  shows "length (signal_shift t r) > 0"
+  unfolding signal_shift_def
+  using assms
+  by auto
+
 lemma signal_shift_valid:
   fixes xs :: "(real \<times> 'v::real_vector) list" and r :: real
   assumes "valid_signal xs" "\<exists>n<length xs. fst (xs!n) = r"
@@ -91,30 +99,30 @@ proof -
     by blast
 qed
 
-fun evals :: "(real \<times> 'v::real_vector) list \<Rightarrow> 'v constraint \<Rightarrow> bool" where
-"evals t (cMu f r) = (f (find_time t 0) > r)"
-| "evals t (cNot c) = (\<not>(evals t c))"
-| "evals t (cAnd c1 c2) = ((evals t c1) \<and> (evals t c2))"
-| "evals t (cUntil x y c1 c2) = (\<exists>t'\<ge>x. t'\<le>y \<and> (\<exists>n<length t. fst (t!n) = t') \<and> evals (signal_shift t t') c2 
-    \<and> (\<forall>t''. t''\<ge>0\<and>t''\<le>t'\<and> (\<exists>n<length t. fst (t!n) = t'') \<longrightarrow> evals (signal_shift t t'') c1))"
+fun evals :: "real \<Rightarrow> (real \<times> 'v::real_vector) list \<Rightarrow> 'v constraint \<Rightarrow> bool" where
+"evals p t (cMu f r) = (if (\<exists>n<length t. fst (t!n) = p) then (f (find_time t p) > r) else False)"
+| "evals p t (cNot c) = (\<not>(evals p t c))"
+| "evals p t (cAnd c1 c2) = ((evals p t c1) \<and> (evals p t c2))"
+| "evals p t (cUntil x y c1 c2) = (\<exists>p'\<ge>p+x. p'\<le>p+y \<and> (\<exists>n<length t. fst (t!n) = p') \<and> evals p' t c2 
+    \<and> (\<forall>p''. p''\<ge>p\<and>p''\<le>p'\<and> (\<exists>n<length t. fst (t!n) = p'') \<longrightarrow> evals p'' t c1))"
 
-lemma cTrue_evals:"evals t cTrue"
+lemma cTrue_evals:"evals p t cTrue = (if (\<exists>n<length t. fst (t!n) = p) then True else False)"
   using cTrue_def evals.simps(1) zero_less_one
   by metis
 
-lemma cOr_evals:"evals t (cOr c1 c2) = (evals t c1 \<or> evals t c2)"
+lemma cOr_evals:"evals p t (cOr c1 c2) = (evals p t c1 \<or> evals p t c2)"
   using cOr_def evals.simps(2,3)
   by metis
 
-lemma cEventually_evals: "evals t (cEventually x y c) =
-    (\<exists>t'\<ge>x. t'\<le>y \<and> (\<exists>n<length t. fst (t!n) = t') \<and> evals (signal_shift t t') c)"
+lemma cEventually_evals: "evals p t (cEventually x y c) =
+    (\<exists>p'\<ge>p+x. p'\<le>p+y \<and> (\<exists>n<length t. fst (t!n) = p') \<and> evals p' t c)"
   using evals.simps(4) cTrue_evals cEventually_def
-  by metis
+  by (smt (verit))
 
-lemma cAlways_evals: "evals t (cAlways x y c) =
-  (\<forall>t'. t'\<ge>x\<and>t'\<le>y\<and> (\<exists>n<length t. fst (t!n) = t') \<longrightarrow> evals (signal_shift t t') c)"
+lemma cAlways_evals: "evals p t (cAlways x y c) =
+  (\<forall>p'. p'\<ge>p+x\<and>p'\<le>p+y\<and> (\<exists>n<length t. fst (t!n) = p') \<longrightarrow> evals p' t c)"
 proof -
-  have "evals t (cAlways x y c) = (\<not>(\<exists>t'\<ge>x. t'\<le>y \<and> (\<exists>n<length t. fst (t!n) = t') \<and> evals (signal_shift t t') (cNot c)))"
+  have "evals p t (cAlways x y c) = (\<not>(\<exists>p'\<ge>p+x. p'\<le>p+y \<and> (\<exists>n<length t. fst (t!n) = p') \<and> evals p' t (cNot c)))"
     using cEventually_evals cAlways_def evals.simps(2)
     by metis
   then show ?thesis
@@ -122,6 +130,7 @@ proof -
     by blast
 qed
 
+(* 
 definition clip_timeline :: "real \<Rightarrow> (real \<times> 'v::real_vector) list \<Rightarrow> (real \<times> 'v) list" where
 "clip_timeline x t = signal_shift t (fst (t!(find_index (\<lambda>r. r\<ge>x) (map fst t))))"
 
@@ -130,33 +139,145 @@ lemma clip_timeline_length:"length (clip_timeline x xs) \<le> length xs"
     order_less_imp_le rev_drop signal_shift_def length_map
   by (metis (no_types, lifting))
 
+lemma clip_timeline_exists:
+  fixes x y :: real and t :: "(real \<times> 'v::real_vector) list"
+  assumes "y\<ge>x" "\<exists>n<length t. fst (t!n) = y"
+  shows "length (clip_timeline x t) > 0"
+proof -
+  have "\<exists>n<length t. fst (t!n) = y"
+    using assms(1,2)
+    by blast
+  then have "\<exists>m<length t. find_index (\<lambda>r. r=y) (map fst t) = m"
+    using find_index_le_size index_def index_first length_map nat_neq_iff nth_map 
+      verit_comp_simplify1(3)
+    by metis
+  then obtain m::nat where m_defn:"m<length t \<and> find_index (\<lambda>r. r\<ge>x) (map fst t) = m"
+    using assms(1) find_index_eq_iff length_map
+    by (metis (mono_tags, lifting))
+  then have "clip_timeline x t = signal_shift t (fst (t!m))"
+    using clip_timeline_def
+    by blast
+  then show ?thesis
+    using signal_shift_exists m_defn
+    by auto
+qed
+
 lemma Until_drop_length:
   assumes "length t > 0"
   shows "length (drop 1 (map (\<lambda>x. (fst x - fst (t!1), snd x)) t)) < length t"
   using assms
   by simp
+*)
+
+lemma cUntil_evals_alt:
+  fixes t :: "(real \<times> 'v::real_vector) list" and c1 c2 :: "'v constraint" and x y :: "real"
+  assumes "valid_signal t"
+  shows "evals t (cUntil x y c1 c2) = (length (clip_timeline x t) > 0 \<and> y\<ge>x 
+    \<and> (evals (clip_timeline x t) c2)
+      \<or> ((evals (clip_timeline x t) c1) 
+        \<and> (evals (drop 1 (map (\<lambda>z. (fst z - fst ((clip_timeline x t)!1), snd z)) (clip_timeline x t))) (cUntil 0 (y-x-fst ((clip_timeline x t)!1)) c1 c2))))"
+proof -
+  {assume "evals t (cUntil x y c1 c2)"
+    then obtain t' where t'_defn:"t'\<ge>x \<and> t'\<le>y \<and> (\<exists>n<length t. fst (t!n) = t') 
+        \<and> evals (signal_shift t t') c2 \<and> (\<forall>t''. t''\<ge>0\<and>t''\<le>t'\<and> (\<exists>n<length t. fst (t!n) = t'') 
+          \<longrightarrow> evals (signal_shift t t'') c1)"
+      using eval.simps(4)
+      by fastforce
+    then have "length (clip_timeline x t) > 0 \<and> y\<ge>x"
+      using clip_timeline_exists 
+      by auto
+
+    oops
+      
 
 value "clip_timeline 2 [(0::real,a),(2,b),(8,c),(12,d),(15,e)]"
 value "length (clip_timeline (fst ([(0,a)]!1)) [(0,a)])"
 value "((clip_timeline x t, c1, \<gamma>), t, cUntil x y c1 c2, \<gamma>)
        \<in> Wellfounded.measure (\<lambda>(t, c, _). size c + length t)"
 
-function robust :: "(real \<times> 'v::real_vector) list \<Rightarrow> 'v constraint \<Rightarrow> real \<Rightarrow> real" where
-"robust t (cMu f r) \<gamma> = f (find_time t 0) - r"
-| "robust t (cNot c) \<gamma> = -(robust t c \<gamma>)"
-| "robust t (cAnd c1 c2) \<gamma> = Min_gamma_comp \<gamma> (robust t c1 \<gamma>) (robust t c2 \<gamma>)"
-| "robust t (cUntil x y c1 c2) \<gamma> = (if length (clip_timeline x t) = 0 \<or> y<0 then -1 else 
+lemma length_filter_implies:
+  assumes "\<forall>n. P n \<longrightarrow> Q n"
+  shows "length (filter P xs) \<le> length (filter Q xs)"
+proof (induct xs)
+  case Nil
+  then show ?case
+    by simp
+next
+  case (Cons a ys)
+  then show ?case
+    using assms 
+    by fastforce
+qed
+
+function robust :: "real \<Rightarrow> (real \<times> 'v::real_vector) list \<Rightarrow> 'v constraint \<Rightarrow> real \<Rightarrow> real" where
+"robust p t (cMu f r) \<gamma> = (if (\<exists>n<length t. fst (t!n) = p) then f (find_time t p) - r else -1)"
+| "robust p t (cNot c) \<gamma> = - (robust p t c \<gamma>)"
+| "robust p t (cAnd c1 c2) \<gamma> = Min_gamma_comp \<gamma> (robust p t c1 \<gamma>) (robust p t c2 \<gamma>)"
+| "robust p t (cUntil x y c1 c2) \<gamma> = (if x<0 \<or> y<0 \<or> ((find (\<lambda>z. fst z \<ge> p+x) t) = None) then -1 else 
       (Max_gamma_comp \<gamma>
-        (robust (clip_timeline x t) c2 \<gamma>)
+        (robust (fst (the (find (\<lambda>z. fst z \<ge> p+x) t))) t c2 \<gamma>)
         (Min_gamma_comp \<gamma> 
-          (robust (clip_timeline x t) c1 \<gamma>)
-          (robust (drop 1 (map (\<lambda>z. (fst z - fst ((clip_timeline x t)!1), snd z)) (clip_timeline x t))) (cUntil 0 (y-x-fst ((clip_timeline x t)!1)) c1 c2) \<gamma>))))"
+          (robust (fst (the (find (\<lambda>z. fst z \<ge> p+x) t))) t c1 \<gamma>)
+          (robust (fst (the (find (\<lambda>z. fst z > (fst (the (find (\<lambda>z. fst z \<ge> p+x) t)))) t))) t 
+            (cUntil 0 (y-x-p) c1 c2) \<gamma>))))"
   by pat_completeness auto
 termination 
   apply (relation 
-      "Wellfounded.measure (\<lambda>(t,c,\<gamma>). size c + length t)")
-         apply simp+
-    apply (simp add: clip_timeline_length less_Suc_eq_le trans_le_add2)
+      "Wellfounded.measure (\<lambda>(p,t,c,\<gamma>). length (filter (\<lambda>x. fst x \<ge> p) t) + size c)")
+        apply simp+
+proof -
+  have "\<And>p t (x::real) y c1.
+       \<not> x < 0 \<and> \<not> y < 0 \<and> (\<exists>a b. find (\<lambda>z. p + x \<le> fst z) t = Some (a, b)) \<Longrightarrow>
+      (\<forall>xa. fst (the (find (\<lambda>z. p + x \<le> fst z) t)) \<le> xa \<longrightarrow> p \<le> xa)"
+    using find_Some_iff2 option.sel
+    by (smt (verit, del_insts))
+  then have 1:"\<And>p t (x::real) y c1.
+     \<not> x < 0 \<and> \<not> y < 0 \<and> (\<exists>a b. find (\<lambda>z. p + x \<le> fst z) t = Some (a, b)) \<Longrightarrow>
+    length (filter (\<lambda>xa. fst (the (find (\<lambda>z. p + x \<le> fst z) t)) \<le> fst xa) t)
+    \<le> length (filter (\<lambda>x. p \<le> fst x) t)"
+    using length_filter_implies find_Some_iff option.sel
+    by (smt (verit, best))
+  then show "\<And>p t (x::real) y c1.
+     \<not> x < 0 \<and> \<not> y < 0 \<and> (\<exists>a b. find (\<lambda>z. p + x \<le> fst z) t = Some (a, b)) \<Longrightarrow>
+      length (filter (\<lambda>xa. fst (the (find (\<lambda>z. p + x \<le> fst z) t)) \<le> fst xa) t)
+       < Suc (length (filter (\<lambda>x. p \<le> fst x) t) + size c1)"
+    by fastforce
+  then have "\<And>(p::real) (t::(real\<times>('v::real_vector)) list) (x::real) (y::real) (c1::'v constraint) (c2::'v constraint) (\<gamma>::real).
+       \<not> (x < 0 \<or> y < 0 \<or> find (\<lambda>z. p + x \<le> fst z) t = None) \<Longrightarrow>
+      length (filter (\<lambda>xa. (fst (the (find (\<lambda>z. p + x \<le> fst z) t))) \<le> fst xa) t) + size c1
+      < length (filter (\<lambda>xa. p \<le> fst xa) t) + size (cUntil x y c1 c2)"
+    using 1 
+    by fastforce
+  then show "\<And>(p::real) (t::(real\<times>('v::real_vector)) list) (x::real) (y::real) (c1::'v constraint) (c2::'v constraint) (\<gamma>::real).
+       \<not> (x < 0 \<or> y < 0 \<or> find (\<lambda>z. p + x \<le> fst z) t = None) \<Longrightarrow>
+       ((fst (the (find (\<lambda>z. p + x \<le> fst z) t)), t, c1, \<gamma>), p, t, cUntil x y c1 c2, \<gamma>)
+       \<in> Wellfounded.measure (\<lambda>(p, t, c, \<gamma>). length (filter (\<lambda>xa. p \<le> fst xa) t) + size c)"
+    by force
+  then have "\<And>(p::real) (t::(real\<times>('v::real_vector)) list) (x::real) (y::real) (c1::'v constraint) (c2::'v constraint) (\<gamma>::real).
+       \<not> (x < 0 \<or> y < 0 \<or> find (\<lambda>z. p + x \<le> fst z) t = None) \<Longrightarrow>
+      size (cUntil 0 (y - x - p) c1 c2) = size (cUntil x y c1 c2)"
+    by auto
+
+  then have "\<And>(p::real) (t::(real\<times>('v::real_vector)) list) (x::real) (y::real) (c1::'v constraint) (c2::'v constraint) (\<gamma>::real).
+       \<not> (x < 0 \<or> y < 0 \<or> find (\<lambda>z. p + x \<le> fst z) t = None) \<Longrightarrow>
+      length (filter (\<lambda>xa. fst (the (find (\<lambda>z. fst (the (find (\<lambda>z. p + x \<le> fst z) t)) < fst z) t)) \<le> fst xa) t) + size (cUntil 0 (y - x - p) c1 c2)
+      < length (filter (\<lambda>xa. p \<le> fst xa) t) + size (cUntil x y c1 c2)"
+    using 1 sledgehammer
+
+  then show "\<And>(p::real) (t::(real\<times>('v::real_vector)) list) (x::real) (y::real) (c1::'v constraint) (c2::'v constraint) (\<gamma>::real).
+       \<not> (x < 0 \<or> y < 0 \<or> find (\<lambda>z. p + x \<le> fst z) t = None) \<Longrightarrow>
+       ((fst (the (find (\<lambda>z. fst (the (find (\<lambda>z. p + x \<le> fst z) t)) < fst z) t)), t,
+         cUntil 0 (y - x - p) c1 c2, \<gamma>),
+        p, t, cUntil x y c1 c2, \<gamma>)
+       \<in> Wellfounded.measure (\<lambda>(p, t, c, \<gamma>). length (filter (\<lambda>xa. p \<le> fst xa) t) + size c)"
+    
+    
+
+    
+    
+    
+    
+
 proof -
   have 1:"\<And>x y \<gamma> t c1 c2.
    \<not> (length (clip_timeline x t) = 0 \<or> y < 0) \<longrightarrow>
@@ -192,5 +313,27 @@ proof -
        \<in> Wellfounded.measure (\<lambda>(t, c, \<gamma>). size c + length t)"
     by auto
 qed
+
+lemma robust_evals_works:
+  fixes t :: "(real \<times> 'v::real_vector) list" and c :: "'v constraint"
+  assumes "valid_signal t"
+  shows "(robust t c 0 > 0) \<longrightarrow> (evals t c) \<and> (robust t c 0 < 0) \<longrightarrow> \<not>(evals t c)"
+proof (induction c)
+  case (cMu f r)
+  then show ?case
+    using robust.simps(1) evals.simps(1)
+    by simp
+next
+  case (cNot c)
+  then show ?case
+    by simp
+next
+  case (cAnd c)
+  then show ?case
+    by argo
+next
+  case (cUntil x y c1 c2)
+  then show ?case
+    oops
 
 end
